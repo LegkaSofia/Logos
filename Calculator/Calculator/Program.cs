c2e1dffc20440138ee9f54fc7ea956e18dcca6ba
Sofiia Lehka <legkasofia@gmail.com>
2022-02-04 20:02:58 +0200

    Create calculator

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            double a;
+            double b;
+            double c;
+
+            static double add(double a, double b)
+            {
+                return a + b;
+            }
+
+            static double minus(double a, double b)
+            {
+                return a - b;
+            }
+
+            static double dilen(double a, double b)
+            {
+                return a / b;
+            }
+
+            static double mnoz(double a, double b)
+            {
+                return a * b;
+            }
+
+            Console.WriteLine("Hello world!");
+
+            Console.WriteLine("Enter a: ");
+            a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter b: ");
+            b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Chose: + - * / ");
+            string action = Console.ReadLine();
+
+
+
+            if (action == "+")
+            {
+                c = add(a, b);
+                Console.WriteLine("Result: ");
+                Console.WriteLine("{0}+{1}={2}", a, b, c);
+            }
+
+
+            if (action == "-")
+            {
+                c = minus(a, b);
+                Console.WriteLine("Result: ");
+                Console.WriteLine("{0}-{1}={2}", a, b, c);
+            }
+
+
+            if (action == "*")
+            {
+                c = mnoz(a, b);
+                Console.WriteLine("Result: ");
+                Console.WriteLine("{0}*{1}={2}", a, b, c);
+            }
+
+
+            if (action == "/")
+            {
+                c = dilen(a, b);
+                if (b == 0)
+                {
+                    Console.WriteLine("Result: ");
+                    Console.WriteLine("Error:( ");
+                }
+                else
+                {
+                    Console.WriteLine("Result: ");
+                    Console.WriteLine("{0}/{1}={2}", a, b, c);
+                }
+            }
+
+            if (action != "+" && action != "-" && action != "*" && action != "/")
+            {
+                Console.WriteLine("Enter the correct character!");
+            }
+
+        }
+    }
+}
