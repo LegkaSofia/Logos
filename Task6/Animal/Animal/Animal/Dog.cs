4943473aa92e6d64b50d7e9794da8ccdd2777c1b
Sofiia Lehka <legkasofia@gmail.com>
2022-02-17 20:34:35 +0200

    Add interfaces

---
+++ Task6/Animal/Animal/Animal/Dog.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animal
+{
+    class Dog : IAnimal
+    {
+        public void Drink()
+        {
+            //throw new NotImplementedException();
+            Console.WriteLine("Dog drinks water");
+        }
+
+        public void Eat()
+        {
+            //throw new NotImplementedException();
+            Console.WriteLine("Dog eats maet");
+        }
+
+        public void Sleep()
+        {
+            //throw new NotImplementedException();
+            Console.WriteLine("Dog sleep");
+        }
+
+        public void Speak()
+        {
+            //throw new NotImplementedException();
+            Console.WriteLine("Dog spek Gav");
+        }
+    }
+}
---
+++ Task6/Animal/Animal/Animal/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            dog.Drink();
+            dog.Eat();
+            dog.Sleep();
+            dog.Speak();
+        }
+    }
+}
