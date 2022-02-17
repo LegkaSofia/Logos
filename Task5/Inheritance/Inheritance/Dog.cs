c3d5a23fbebf8ddb32aca9b87ed296a845efee18
Sofiia Lehka <legkasofia@gmail.com>
2022-02-17 18:14:21 +0200

    Add inheritance

---
+++ Task5/Inheritance/Inheritance/Dog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    class Dog : Animal
+    {
+        public override void Say()
+        {
+            Console.WriteLine("Собака каже Гав");
+        }
+        
+    }
+}
