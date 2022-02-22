09dafb39d7e9424927533d3bde98d16e5f690be7
Sofiia Lehka <legkasofia@gmail.com>
2022-02-22 22:08:55 +0200

    Add school grades

--- Task7/Grades/Grades/Program.cs
+++ Task7/Grades/Grades/Program.cs
@@ -6,9 +6,13 @@ namespace Grades
     {
         static void Main(string[] args)
         {
-            Classroom classroom = new Classroom();
-            double grade = classroom.TotalGrade();
-            Console.WriteLine("Total Grade: {0}", grade);
+           // Classroom classroom = new Classroom();
+            //double grade = classroom.TotalGrade();
+            //Console.WriteLine("Total Grade: {0}", grade);
+
+            School school = new School();
+            double grade1 = school.TotalGrade();
+            Console.WriteLine("Total Grade: {0}", grade1);
         }
     }
 }
---
+++ Task7/Grades/Grades/School.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Grades
+{
+    class School
+    {
+        public Classroom[] Classrooms { get; set; }
+        public School()
+        {
+            Classrooms = new Classroom[]
+            {
+                new Classroom(),
+                new Classroom(),
+                new Classroom()
+            };     
+        }
+
+        public double TotalGrade()
+        {
+            double sum = 0;
+
+            for (int i = 0; i <Classrooms.Length ; i++)
+            {
+                sum = sum + Classrooms[i].TotalGrade();
+            }
+            return sum;
+        }
+    }
+}
