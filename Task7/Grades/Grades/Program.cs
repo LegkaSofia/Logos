1aa902809f6426a8bc72d17b4ec2735dffdfa31a
Sofiia Lehka <legkasofia@gmail.com>
2022-02-22 21:49:38 +0200

    add classroom grades

---
+++ Task7/Grades/Grades/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Classroom classroom = new Classroom();
+            double grade = classroom.TotalGrade();
+            Console.WriteLine("Total Grade: {0}", grade);
+        }
+    }
+}
