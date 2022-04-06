223d64b580e0fb7bdb31cc8d6eb207aca4fd8d93
Sofiia Lehka <legkasofia@gmail.com>
2022-04-06 20:36:05 +0300

    virtual methods

---
+++ Task8/Virtuals/Virtuals/PostProduct.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Virtuals
+{
+    class PostProduct : Product
+    {
+        public double TransferPrice { get; set; }
+
+        public override double TotalPrice()
+        {
+            return TransferPrice + Price;
+        }
+    }
+}
---
+++ Task8/Virtuals/Virtuals/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Virtuals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product() { Price = 67.94};
+            Product product1 = new PostProduct() { Price = 76.52, TransferPrice = 10 };
+
+            Product[] products = new Product[] { product, product1 };
+
+            foreach (var item in products)
+            {
+                Console.WriteLine(item.TotalPrice());
+            }
+        }
+    }
+}
