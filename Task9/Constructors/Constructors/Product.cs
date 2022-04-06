24c14b71b82f489ea1f49725c853fbe41fa66cd8
Sofiia Lehka <legkasofia@gmail.com>
2022-04-06 20:51:27 +0300

    constructors

---
+++ Task9/Constructors/Constructors/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Constructors
+{
+    class Product
+    {
+
+        public string Name { get; set;}
+        public double Price { get; set; }
+
+        public Product()
+        {
+        }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public Product(string name)
+        {
+            Name = name;
+        }
+
+        public Product(double price)
+        {
+            Price = price;
+        }
+
+    }
+}
---
+++ Task9/Constructors/Constructors/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product("Apple", 11);
+            Product product1 = new Product(15);
+        }
+    }
+}
