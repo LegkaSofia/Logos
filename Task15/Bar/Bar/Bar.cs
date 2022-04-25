329e42b2091361e9ae21fd38bd86b502485ea023
Sofiia Lehka <legkasofia@gmail.com>
2022-04-25 21:10:01 +0300

    bar

---
+++ Task15/Bar/Bar/Bar.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace Bar
+{
+    internal class Bar
+    {
+        public Bar(char symbol)
+        {
+            Symbol = symbol;
+        }
+
+        public char Symbol { get; set; }
+
+        public void Start()
+        {
+            var width = Console.WindowWidth;
+            for (int i = 0; i < width; i++)
+            {
+                Console.Write(Symbol);
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
